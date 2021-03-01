d38063eccc9a25bc152e39549c7600c8d1391af7
YmCoke <1272698173@qq.com>
2021-03-01 12:34:36 +0800

    fix: 修复了上传图片格式的验证配置

--- src/menus/img/upload-img.ts
+++ src/menus/img/upload-img.ts
@@ -116,7 +116,6 @@ class UploadImg {
                 return
             }
         }
-
         // ------------------------------ 验证文件信息 ------------------------------
         const resultFiles: File[] = []
         const errInfos: string[] = []
@@ -129,7 +128,11 @@ class UploadImg {
                 return
             }
 
-            if (/\.(jpg|jpeg|png|bmp|gif|webp)$/i.test(name) === false) {
+            // 将uploadImgAccept数组转换为正则对象
+            const imgType = editor.config.uploadImgAccept.join('|')
+            const imgTypeRuleStr = `.(${imgType})$`
+            const uploadImgAcceptRule = new RegExp(imgTypeRuleStr, 'i')
+            if (uploadImgAcceptRule.test(name) === false) {
                 // 后缀名不合法，不是图片
                 errInfos.push(`【${name}】${t('不是图片')}`)
                 return
